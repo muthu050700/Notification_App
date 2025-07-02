c0f9613c0b991a49c4bbe2316403bd417b8a5eb8
Santhosh Sivaji <santhosh_s@ambattur.com>
2025-07-02 18:36:21 +0530

    bug fixes

--- src/Components/NotificationList/NotificationList.jsx
+++ src/Components/NotificationList/NotificationList.jsx
@@ -9,10 +9,10 @@ const NotificationList = () => {
         <div className=" bg-[#f1faee] min-h-[100vh] max-h-full ">
             <h1 className=" font-bold text-2xl text-center text-[#F1EFEC]">Notivication List</h1>
             <NotificationCart />
-            <div className=" flex justify-center">
-                <div className="fixed w-full bottom-0 overflow-hidden bg-[#ebeaea] p-2 shadow-lg rounded-2xl">
+            <div className=" flex justify-center ">
+                <div className="fixed w-11/12 bottom-2 overflow-hidden bg-[#ebeaea] p-1 shadow-lg rounded-2xl">
                     <div className="flex justify-between bg-[#ebeaea] rounded-[10px]">
-                        <button className="bg-[#578E7E] text-[#000000] rounded-2xl w-6/12 text-lg font-bold px-2 py-2" style={{ borderRadius: "10px" }}>Approve</button>
+                        <button className="bg-[#578E7E] text-[#000000] w-6/12 text-lg font-bold px-2 py-2" style={{ borderRadius: "10px" }}>Approve</button>
                         <button className="text-[#000000] text-lg w-6/12 font-bold px-2 py-2">Reject</button>
                     </div>
                 </div>
